2af00a63b4d93dd4f4d918b535c39c0d8442eb3e
mmhuntsberry <mmhuntsberry@gmail.com>
2023-07-06 11:58:28 -0500

    chore: add react storybook and sd transforms

--- packages/tokens/build-tokens.js
+++ packages/tokens/build-tokens.js
@@ -5,11 +5,34 @@ registerTransforms(StyleDictionary, {
   excludeParentKeys: true,
 });
 
+StyleDictionary.registerTransform({
+  name: "toolkit/radii/px",
+  type: "value",
+  matcher: function (token) {
+    return token.attributes.category === "radii";
+  },
+  transformer: function (token) {
+    return token.original.value + "px";
+  },
+});
+
+StyleDictionary.registerTransform({
+  name: "toolkit/fontSize/rem",
+  type: "value",
+  matcher: function (token) {
+    return token.attributes.category === "fontSize";
+  },
+  transformer: function (token) {
+    return token.original.value;
+  },
+});
+
 const sd = StyleDictionary.extend({
   source: ["*.json"],
   platforms: {
     css: {
       transforms: [
+        "attribute/cti",
         "ts/descriptionToComment",
         "ts/size/px",
         "ts/opacity",
@@ -23,6 +46,8 @@ const sd = StyleDictionary.extend({
         "ts/color/css/hexrgba",
         "ts/color/modifiers",
         "name/cti/kebab",
+        "toolkit/radii/px",
+        "toolkit/fontSize/rem",
       ],
       buildPath: "build/css/",
       files: [
