97cab057c6146f0d3222591b33dae45cbf55154d
mmhuntsberry <mmhuntsberry@gmail.com>
2023-07-13 15:47:33 -0500

    chore: resolve merge conflict

---
+++ packages/components/src/lib/button/button.stories.tsx
@@ -0,0 +1,14 @@
+import Button from "./button";
+
+export default {
+  component: Button,
+  title: "Button",
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    children: "Button Text",
+    type: "primary",
+  },
+};
---
+++ packages/components/src/lib/button/button.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "@emotion/styled";
+import "@ghostkit/tokens";
+
+type ButtonProps = {
+  variant: "primary" | "secondary" | "tertiary";
+  state?: "default" | "hover" | "focused" | "disabled";
+  children?: React.ReactNode;
+};
+
+const StyledButton = styled.button<ButtonProps>`
+  * {
+    box-sizing: border-box;
+  }
+  align-items: center;
+  background: ${(props) =>
+    props.variant === "primary" && "var(--theme-accent-default)"};
+  /* border: ${(props) =>
+    props.variant === "primary" && `1px solid var(--theme-accent-default)`}; */
+  border: none;
+  border-radius: 0.25rem;
+  color: ${(props) =>
+    props.variant === "primary" && "var(--theme-fg-on-accent)"};
+  cursor: pointer;
+  display: flex;
+  font: var(--typography-button-normal-bold);
+  justify-content: center;
+  /* letter-spacing: -0.00813rem; */
+  max-height: 2.5rem;
+  outline: none;
+  padding: 12px;
+  text-transform: uppercase;
+`;
+
+const Button: React.FC<ButtonProps> = ({ children, variant = "primary" }) => {
+  return <StyledButton variant={variant}>{children}</StyledButton>;
+};
+
+export default Button;
