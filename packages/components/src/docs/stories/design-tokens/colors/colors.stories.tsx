645492e389eada76936df27f1ec5c213815c932a
mmhuntsberry <mmhuntsberry@gmail.com>
2023-08-23 06:19:56 -0500

    chore: add sizing

--- packages/components/src/docs/stories/design-tokens/colors/colors.stories.tsx
+++ packages/components/src/docs/stories/design-tokens/colors/colors.stories.tsx
@@ -34,10 +34,6 @@ const variants = [
   "950",
 ];
 
-// export const Grey = {
-//   render: () => <h2>Hello world</h2>,
-// };
-
 export const Grey = {
   args: {
     color: "Grey",
@@ -75,76 +71,76 @@ export const Grey = {
   ),
 };
 
-// export const Red = {
-//   args: {
-//     color: "Red",
-//   },
-//   render: (args) => (
-//     <Table>
-//       <TableCaption>{args.color}</TableCaption>
-//       <TableHeader>
-//         <TableRow>
-//           <TableHead className="w-[100px]">Variant</TableHead>
-//           <TableHead>Variable</TableHead>
-//           <TableHead className="text-right">Swatch</TableHead>
-//         </TableRow>
-//       </TableHeader>
-//       <TableBody>
-//         {variants.map((v) => (
-//           <TableRow key={`${args.color.toLowerCase()}-${v}`}>
-//             <TableCell className="font-medium">{v}</TableCell>
-//             <TableCell>
-//               --colors-{args.color.toLowerCase()}-{v}
-//             </TableCell>
-//             <TableCell className="float-right">
-//               <div
-//                 style={{
-//                   width: "24px",
-//                   height: "24px",
-//                   backgroundColor: `var(--colors-${args.color.toLowerCase()}-${v})`,
-//                 }}
-//               ></div>
-//             </TableCell>
-//           </TableRow>
-//         ))}
-//       </TableBody>
-//     </Table>
-//   ),
-// };
+export const Red = {
+  args: {
+    color: "Red",
+  },
+  render: (args) => (
+    <Table>
+      <TableCaption>{args.color}</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Variant</TableHead>
+          <TableHead>Variable</TableHead>
+          <TableHead className="text-right">Swatch</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {variants.map((v) => (
+          <TableRow key={`${args.color.toLowerCase()}-${v}`}>
+            <TableCell className="font-medium">{v}</TableCell>
+            <TableCell>
+              --colors-{args.color.toLowerCase()}-{v}
+            </TableCell>
+            <TableCell className="float-right">
+              <div
+                style={{
+                  width: "24px",
+                  height: "24px",
+                  backgroundColor: `var(--colors-${args.color.toLowerCase()}-${v})`,
+                }}
+              ></div>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  ),
+};
 
-// export const Blue = {
-//   args: {
-//     color: "Blue",
-//   },
-//   render: (args) => (
-//     <Table>
-//       <TableCaption>{args.color}</TableCaption>
-//       <TableHeader>
-//         <TableRow>
-//           <TableHead className="w-[100px]">Variant</TableHead>
-//           <TableHead>Variable</TableHead>
-//           <TableHead className="text-right">Swatch</TableHead>
-//         </TableRow>
-//       </TableHeader>
-//       <TableBody>
-//         {variants.map((v) => (
-//           <TableRow key={`${args.color.toLowerCase()}-${v}`}>
-//             <TableCell className="font-medium">{v}</TableCell>
-//             <TableCell>
-//               --colors-{args.color.toLowerCase()}-{v}
-//             </TableCell>
-//             <TableCell className="float-right">
-//               <div
-//                 style={{
-//                   width: "24px",
-//                   height: "24px",
-//                   backgroundColor: `var(--colors-${args.color.toLowerCase()}-${v})`,
-//                 }}
-//               ></div>
-//             </TableCell>
-//           </TableRow>
-//         ))}
-//       </TableBody>
-//     </Table>
-//   ),
-// };
+export const Blue = {
+  args: {
+    color: "Blue",
+  },
+  render: (args) => (
+    <Table>
+      <TableCaption>{args.color}</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Variant</TableHead>
+          <TableHead>Variable</TableHead>
+          <TableHead className="text-right">Swatch</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {variants.map((v) => (
+          <TableRow key={`${args.color.toLowerCase()}-${v}`}>
+            <TableCell className="font-medium">{v}</TableCell>
+            <TableCell>
+              --colors-{args.color.toLowerCase()}-{v}
+            </TableCell>
+            <TableCell className="float-right">
+              <div
+                style={{
+                  width: "24px",
+                  height: "24px",
+                  backgroundColor: `var(--colors-${args.color.toLowerCase()}-${v})`,
+                }}
+              ></div>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  ),
+};
