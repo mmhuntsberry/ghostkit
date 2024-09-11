bfb1ece6b0c5561d25a5b420c1b5a3380dfc5e03
mmhuntsberry <mmhuntsberry@gmail.com>
2024-09-10 22:04:17 -0500

    fix: ts error

--- packages/components/.storybook/preview.tsx
+++ packages/components/.storybook/preview.tsx
@@ -3,12 +3,13 @@ import "../../../node_modules/@mmhuntsberry/tokens/build/css/index.css";
 import "@mmhuntsberry/tokens/themes/white-label";
 import "@mmhuntsberry/tokens/themes/bicycling";
 // import "@mmhuntsberry/fonts";
-import React, { useEffect, useContext } from "react";
-import { Meta, StoryContext } from "@storybook/react";
+import React, { useEffect } from "react";
+import { StoryContext } from "@storybook/react";
 
 export const BrandContext = React.createContext("white-label");
 
 export const decorators = [
+  //@ts-expect-error StoryContext is not typed correctly
   (Story, context: StoryContext) => {
     useEffect(() => {
       const brand = context.globals.brand;
