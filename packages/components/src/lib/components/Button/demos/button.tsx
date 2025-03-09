77a7fcec7bfd6ff3ac0f7789da9aed78307d0203
mmhuntsberry <mmhuntsberry@gmail.com>
2025-03-09 17:59:16 -0500

    fix conflict

--- packages/components/src/lib/components/Button/demos/button.tsx
+++ packages/components/src/lib/components/Button/demos/button.tsx
@@ -2,10 +2,12 @@ import React, { useContext } from "react";
 
 import { Button } from "../Button";
 
-export const Demo = (props) => {
+export const Demo = (props: any) => {
   return (
     <div>
       <Button {...props}>{props.children}</Button>
     </div>
   );
 };
+
+export default Demo;
