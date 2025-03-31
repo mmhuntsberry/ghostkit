9f178ad2354d10220fb9ec08442e2801ea4fb393
mmhuntsberry <mmhuntsberry@gmail.com>
2025-03-31 12:52:30 -0500

    add tokens poc

--- packages/tokens/config.mjs
+++ packages/tokens/config.mjs
@@ -2,12 +2,35 @@ import StyleDictionary from "style-dictionary";
 
 const isAliasToken = (filePath) => filePath.includes("alias");
 
-// 1) Register custom format
+// ✅ Match tokens at path root === "size"
+StyleDictionary.registerTransform({
+  name: "value/size/px",
+  type: "value",
+
+  filter: (token) => {
+    console.log("token path", token.path[0]);
+
+    return token.path[0] === "size";
+  },
+  transform: (token) => `${token.value}px`,
+});
+
+StyleDictionary.registerTransform({
+  name: "value/font-size/rem",
+  type: "value",
+  filter: (token) => token.path[0] === "font" && token.path[1] === "size",
+  transform: (token) => `${parseFloat(token.value) / 16}rem`,
+});
+
+StyleDictionary.registerTransform({
+  name: "name/cti/kebab",
+  type: "name",
+  transform: (token) => token.path.join("-"),
+});
+
 StyleDictionary.registerFormat({
   name: "custom/custom-variable",
   format: function ({ dictionary, platform }) {
-    console.log("All tokens =>", dictionary.allTokens); // debug
-
     return `
 :root,
 :host {
@@ -25,14 +48,18 @@ ${dictionary.allTokens
   },
 });
 
-// 2) Export config
 export default {
-  // Important: includes all subfolders in src/
   source: ["src/**/*.json"],
   platforms: {
     css: {
       prefix: "ds",
-      transformGroup: "css",
+      // transformGroup: "css",
+      transforms: [
+        "attribute/cti",
+        "name/cti/kebab",
+        "value/size/px",
+        "value/font-size/rem",
+      ],
       buildPath: "./build/css/",
       files: [
         {
