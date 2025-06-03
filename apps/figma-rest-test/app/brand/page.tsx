6206d75fc0157d745069a8dc4fc83a20422dc309
mmhuntsberry <mmhuntsberry@gmail.com>
2025-06-03 12:00:28 -0500

    fest(figma-rest-test): add tokens

---
+++ apps/figma-rest-test/app/brand/page.tsx
@@ -0,0 +1,20 @@
+// File: apps/figma-rest-test/app/brand/page.tsx
+
+"use client";
+
+export default function BrandPage() {
+  return (
+    <div className="p-6 space-y-8">
+      <button
+        style={{
+          backgroundColor:
+            "var(--palette-action-background-primary-solid-default, blue)",
+          paddingInline: "var(--space-md)",
+          paddingBlock: "var(--space-xs)",
+        }}
+      >
+        Hiya
+      </button>
+    </div>
+  );
+}
