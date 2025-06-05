f513ddbbe5b908f092296c64576cd1c936c3e0ab
mmhuntsberry <mmhuntsberry@gmail.com>
2025-06-05 14:44:27 -0500

    add color and radius to demo

--- apps/figma-rest-test/app/brand/page.tsx
+++ apps/figma-rest-test/app/brand/page.tsx
@@ -9,6 +9,8 @@ export default function BrandPage() {
         style={{
           backgroundColor:
             "var(--palette-action-background-primary-solid-default, blue)",
+          color: "var(--palette-action-content-primary-solid-default, white)",
+          borderRadius: "var(--container-action-radius-default, 4px)",
           paddingInline: "var(--space-md)",
           paddingBlock: "var(--space-xs)",
         }}
